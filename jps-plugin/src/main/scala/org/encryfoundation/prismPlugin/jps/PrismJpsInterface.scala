b9021f3b4946efae58578b8fb6c1001298e91b2c
aleksandr <alexbromel@gmail.com>
2019-09-03 09:06:15 +0300

    add using compiler as library

--- jps-plugin/src/main/scala/org/encryfoundation/prismPlugin/jps/PrismJpsInterface.scala
+++ jps-plugin/src/main/scala/org/encryfoundation/prismPlugin/jps/PrismJpsInterface.scala
@@ -3,15 +3,12 @@ package org.encryfoundation.prismPlugin.jps
 import java.io.File
 
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.process.{CapturingProcessHandler, OSProcessHandler, ProcessOutput}
-import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.roots.impl.OrderEntryUtil
-import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
-import org.apache.commons.io.FilenameUtils
-import org.jetbrains.jps.incremental.{CompileContext, ProjectBuildException}
-import org.encryfoundation.prismPlugin.model.{JpsPrismModuleType, JpsPrismSdkType}
-import org.jetbrains.jps.incremental.messages.{BuildMessage, CompilerMessage}
+import com.intellij.openapi.application.ex.PathManagerEx
+import com.intellij.openapi.roots.ProjectRootManager
+import org.encryfoundation.prismPlugin.model.JpsPrismSdkType
+import org.jetbrains.jps.incremental.CompileContext
 import org.jetbrains.jps.model.module.JpsModule
+import org.jetbrains.jps.model.library._
 
 case class PrismJpsInterface private (rootDir: File, comilerFileName: File) {
 
@@ -39,12 +36,25 @@ object PrismJpsInterface {
 
   def apply(module: JpsModule, context: CompileContext): PrismJpsInterface = {
     val moduleRoot = module.getContentRootsList.getUrls.get(0).substring("file://".length)
+    context.getProjectDescriptor
+      .getProjectJavaSdks
+      .asScala
+      .foreach(sdk => println(s"Sdk: ${sdk.getHomePath}"))
     println("libs:" + module.getLibraryCollection)
     val rootDir = new File(moduleRoot)
-    println(s"rootDir: $rootDir")
-    println(s"sdks in project: ${module.getProject.getLibraryCollection.getLibraries.asScala.map(lib => lib.toString).mkString(",")}")
+    val compilerPath = module.getProject
+      .getLibraryCollection
+      .getLibraries
+      .asScala.filter { lib =>
+        println(s"libName: ${lib.createReference().getLibraryName}")
+        lib.createReference().getLibraryName == "Prism"
+      }
+      .flatMap { lib =>
+        lib.getFiles(JpsOrderRootType.COMPILED).asScala
+      }
+      .head
     val sdk = module.getSdk(JpsPrismSdkType.instance)
     println(s"sdk: ${sdk}")
-    new PrismJpsInterface(rootDir, new File("/Users/aleksandr/IdeaProjects/PrismLang/target/scala-2.12/Prism.jar"))
+    new PrismJpsInterface(rootDir, compilerPath)
   }
 }
